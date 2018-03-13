48bc26015ed478382efab8551cd3e83e053d5b3a
fool <chrism@netlify.com>
2018-03-13 15:44:54 -0700

    syntax error

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,6 @@
 module.exports = function(grunt) {
 
   // A very basic default task.
-  grunt.registerTask('default', 'Log some stuff.', function() {
-    grunt.log.write('My name is Grunt and I'm logging some stuff...').ok();
-  });
+  grunt.registerTask('default', 'Log some stuff.', function() { grunt.log.write('My name is Grunt and I\'m logging some stuff...').ok();});
 
 };

b5976c73e6d6a30c4b67e7fecf6a34de18dcc285
fool <chrism@netlify.com>
2021-10-11 15:53:21 -0700

    updating URL to test again

--- test-node-ssl.js
+++ test-node-ssl.js
@@ -1,6 +1,6 @@
 var axios = require('axios')
 
-axios.get('https://github-bat.v7apps.com/Canada/vuse.com/')
+axios.get('https://en.wikipedia.org/wiki/List_of_Masters_Tournament_champions')
       .then(function (res) {
           console.log(res);
           // ... do something with the response
